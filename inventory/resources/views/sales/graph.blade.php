841fc21e9cfe8484cf6c9aa94a2a4944c13a87ce
ArugaYudish <ayudish7@gmail.com>
2023-07-24 12:07:14 +0700

    update graph

--- inventory/resources/views/sales/graph.blade.php
+++ inventory/resources/views/sales/graph.blade.php
@@ -1,44 +1,53 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="bg-white py-4 mb-5">
+        <div class="border-bottom p-3" style="width: 80%; margin: auto;">
+            <form onsubmit="return false" class="row">
+                <div class="col-sm-4 text-center ">
+                    Start Date
+                    <input class="w-100 px-2 py-1" style="border-radius:20px; border:1px solid grey;" type="date" id="startDate">
+                </div>
+                <div class="col-sm-4 text-center ">
+                    End Date
+                    <input class="w-100 px-2 py-1" style="border-radius:20px; border:1px solid grey;" type="date" id="endDate">
+                </div>
+                <div class="col-sm-4">
+                    <button class=" btn btn-primary float-right mt-3"  onclick="drawCustomChart()">Show Custom Chart</button>
+                </div>
+            </form>
+        </div>
+        <div style="width: 80%; margin: auto;" >
+            <p  class="mt-2" style="color:blue;" >Total Amount: <span id="totalAmount">-</span></p>
+        </div>
+        <div style="width: 80%; margin: auto;">
+            <canvas id="monthlySalesChart"></canvas>
+        </div>
 
-    <div style="width: 80%; margin: auto;">
-        <canvas id="monthlySalesChart"></canvas>
+        <div style="width: 80%; margin: auto;">
+            <canvas id="threeMonthsSalesChart"></canvas>
+        </div>
+
+        <div style="width: 80%; margin: auto;">
+            <canvas id="oneYearSalesChart"></canvas>
+        </div>
+
+        <div style="width: 80%; margin: auto;">
+            <canvas id="customSalesChart"></canvas>
+        </div>
+
     </div>
 
-    <div style="width: 80%; margin: auto;">
-        <canvas id="threeMonthsSalesChart"></canvas>
-    </div>
 
-    <div style="width: 80%; margin: auto;">
-        <canvas id="oneYearSalesChart"></canvas>
-    </div>
-
-    <div style="width: 80%; margin: auto;">
-        <canvas id="customSalesChart"></canvas>
-    </div>
-
-    <div style="width: 80%; margin: auto;">
-        <form onsubmit="return false">
-          <label for="startDate">Start Date:</label>
-          <input type="date" id="startDate">
-          <label for="endDate">End Date:</label>
-          <input type="date" id="endDate">
-          <button onclick="drawCustomChart()">Show Custom Chart</button>
-        </form>
-      <p>Total Amount: <span id="totalAmount">-</span></p>
-    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
     <script>
-
-        
         var monthlySalesData = @json($monthlySales);
         var threeMonthsSalesData = @json($threeMonthsSales);
         var oneYearSalesData = @json($oneYearSales);
 
-        
+
         var formatData = function(salesData) {
             return {
                 labels: salesData.map(item => item.issue_date),
@@ -46,7 +55,7 @@
             };
         };
 
-        
+
         var drawChart = function(ctx, chartData, label) {
             new Chart(ctx, {
                 type: 'line',
@@ -70,46 +79,49 @@
             });
         };
 
-        
+
         var ctxMonthlySales = document.getElementById('monthlySalesChart').getContext('2d');
         var monthlySalesChartData = formatData(monthlySalesData);
         drawChart(ctxMonthlySales, monthlySalesChartData, 'Penjualan 1 Bulan Terakhir');
 
-        
+
         var ctxThreeMonthsSales = document.getElementById('threeMonthsSalesChart').getContext('2d');
         var threeMonthsSalesChartData = formatData(threeMonthsSalesData);
         drawChart(ctxThreeMonthsSales, threeMonthsSalesChartData, 'Penjualan 3 Bulan Terakhir');
 
-        
+
         var ctxOneYearSales = document.getElementById('oneYearSalesChart').getContext('2d');
         var oneYearSalesChartData = formatData(oneYearSalesData);
         drawChart(ctxOneYearSales, oneYearSalesChartData, 'Penjualan 1 Tahun Terakhir');
 
         var drawCustomChart = function() {
-        var startDate = document.getElementById('startDate').value;
-        var endDate = document.getElementById('endDate').value;
+            var startDate = document.getElementById('startDate').value;
+            var endDate = document.getElementById('endDate').value;
 
-        
-        fetch('/getSalesDataCustom', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRF-TOKEN': '{{ csrf_token() }}',
-            },
-            body: JSON.stringify({ startDate: startDate, endDate: endDate }),
-        })
-        .then(response => response.json())
-        .then(data => {
-            var customSalesChartData = formatData(data.salesData);
-            var ctxCustomSales = document.getElementById('customSalesChart').getContext('2d');
-            drawChart(ctxCustomSales, customSalesChartData, `Custom Sales (${startDate} to ${endDate})`);
 
-            
-            document.getElementById('totalAmount').innerText = data.totalAmount;
-        })
-        .catch(error => {
-            console.error('Error fetching custom sales data:', error);
-        });
-    };
+            fetch('/getSalesDataCustom', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-CSRF-TOKEN': '{{ csrf_token() }}',
+                    },
+                    body: JSON.stringify({
+                        startDate: startDate,
+                        endDate: endDate
+                    }),
+                })
+                .then(response => response.json())
+                .then(data => {
+                    var customSalesChartData = formatData(data.salesData);
+                    var ctxCustomSales = document.getElementById('customSalesChart').getContext('2d');
+                    drawChart(ctxCustomSales, customSalesChartData, `Custom Sales (${startDate} to ${endDate})`);
+
+
+                    document.getElementById('totalAmount').innerText = data.totalAmount;
+                })
+                .catch(error => {
+                    console.error('Error fetching custom sales data:', error);
+                });
+        };
     </script>
 @endsection
