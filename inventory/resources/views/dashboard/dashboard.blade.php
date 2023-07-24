adf1bb4adce6510019fa963979e1e7dc3a1e65d5
ArugaYudish <ayudish7@gmail.com>
2023-07-24 23:06:43 +0700

    Updates

--- inventory/resources/views/dashboard/dashboard.blade.php
+++ inventory/resources/views/dashboard/dashboard.blade.php
@@ -3,6 +3,8 @@
 @section('content')
     <div class="container">
 
+
+
         <div class="row">
 
             <div class="col-xl-4 col-md-6 mb-4">
@@ -11,8 +13,8 @@
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
                                 <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                    Earnings (Monthly)</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                                    Total Customer</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $customerCount }}</div>
                             </div>
                             <div class="col-auto">
                                 <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -28,8 +30,40 @@
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
                                 <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                    Earnings (Annual)</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                    Total Sales</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalSalesThisMonth }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-md-6 mb-4">
+                <div class="card border-left-success shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                    Sold Products</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $soldProductsThisMonth }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-md-6 mb-4">
+                <div class="card border-left-success shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                    Total Products</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalProductionThisMonth }}</div>
                             </div>
                             <div class="col-auto">
                                 <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -40,126 +74,137 @@
             </div>
 
             <!-- Earnings (Monthly) Card Example -->
+
+
             <div class="col-xl-4 col-md-6 mb-4">
-                <div class="card border-left-info shadow h-100 py-2">
+                <div class="card border-left-warning shadow h-100 py-2">
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
-                                </div>
-                                <div class="row no-gutters align-items-center">
-                                    <div class="col-auto">
-                                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
-                                    </div>
-                                    <div class="col">
-                                        <div class="progress progress-sm mr-2">
-                                            <div class="progress-bar bg-info" role="progressbar" style="width: 50%"
-                                                aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                                        </div>
-                                    </div>
-                                </div>
+                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                    Pending Requests</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
                             </div>
                             <div class="col-auto">
-                                <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                <i class="fas fa-comments fa-2x text-gray-300"></i>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-xl-8 col-md-6 mb-4 row ml-3">
-                
+            <div class="col-xl-4 col-md-6 mb-4">
+                <div class="card border-left-warning shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                    Pending Requests</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-comments fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
-            <!--  Pending Requests Card Example -->
-            <div class="col-xl-4 col-md-6 mb-4 row ">
-                <div class="col-12  mb-4">
-                    <div class="card border-left-warning shadow h-100 py-2">
-                        <div class="card-body">
-                            <div class="row no-gutters align-items-center">
-                                <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                        Pending Requests</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
-                                </div>
-                                <div class="col-auto">
-                                    <i class="fas fa-comments fa-2x text-gray-300"></i>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12 mb-4">
-                    <div class="card border-left-warning shadow h-100 py-2">
-                        <div class="card-body">
-                            <div class="row no-gutters align-items-center">
-                                <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                        Pending Requests</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
-                                </div>
-                                <div class="col-auto">
-                                    <i class="fas fa-comments fa-2x text-gray-300"></i>
-                                </div>
-                            </div>
-                        </div>
+            <div class="col-xl-8 col-md-6 mb-4 row ">
+                <div class="col-12 mt-1  h-100 " margin: auto;">
+                    <div class="border p-2 " style="border-radius:8px;"><canvas id="monthlySalesChart"></canvas>
                     </div>
                 </div>
             </div>
+
+
 
         </div>
     </div>
 
 
 
-    <div class="row justify-content-center mt-5">
-        <div class="w-100 ">
 
 
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-            <div class="card">
-                <div class="card-header">Dashboard baru</div>
-                <div class="card-body">
-
-
-                    <div class="card mt-3">
-                        <div class="card-header">Total Customers</div>
-                        <div class="card-body">
-                            <h1>{{ $customerCount }}</h1>
-                        </div>
-                    </div>
-
-                    <div class="card mt-3">
-                        <div class="card-header">stock available</div>
-                        <div class="card-body">
-                            <h1>{{ $totalStock }}</h1>
-                        </div>
-                    </div>
-
-                    <div class="card mt-3">
-                        <div class="card-header">Sold Products This Month</div>
-                        <div class="card-body">
-                            <h1>{{ $soldProductsThisMonth }}</h1>
-                        </div>
-                    </div>
-
-                    <div class="card mt-3">
-                        <div class="card-header">Total Sales This Month</div>
-                        <div class="card-body">
-                            <h1>{{ $totalSalesThisMonth }}</h1>
-                        </div>
-                    </div>
-
-                    <div class="card mt-3">
-                        <div class="card-header">Total Production This Month</div>
-                        <div class="card-body">
-                            <h1>{{ $totalProductionThisMonth }}</h1>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    </div>
+    <script>
+        var monthlySalesData = @json($monthlySales);
 
 
+        var formatData = function(salesData) {
+            return {
+                labels: salesData.map(item => item.issue_date),
+                data: salesData.map(item => item.total_amount),
+            };
+        };
+
+
+        var drawChart = function(ctx, chartData, label) {
+            new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: chartData.labels,
+                    datasets: [{
+                        label: label,
+                        data: chartData.data,
+                        borderColor: 'rgba(75, 192, 192, 1)',
+                        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+        };
+
+
+        var ctxMonthlySales = document.getElementById('monthlySalesChart').getContext('2d');
+        var monthlySalesChartData = formatData(monthlySalesData);
+        drawChart(ctxMonthlySales, monthlySalesChartData, 'Penjualan 1 Bulan Terakhir');
+
+
+        var ctxThreeMonthsSales = document.getElementById('threeMonthsSalesChart').getContext('2d');
+        var threeMonthsSalesChartData = formatData(threeMonthsSalesData);
+        drawChart(ctxThreeMonthsSales, threeMonthsSalesChartData, 'Penjualan 3 Bulan Terakhir');
+
+
+        var ctxOneYearSales = document.getElementById('oneYearSalesChart').getContext('2d');
+        var oneYearSalesChartData = formatData(oneYearSalesData);
+        drawChart(ctxOneYearSales, oneYearSalesChartData, 'Penjualan 1 Tahun Terakhir');
+
+        var drawCustomChart = function() {
+            var startDate = document.getElementById('startDate').value;
+            var endDate = document.getElementById('endDate').value;
+
+
+            fetch('/getSalesDataCustom', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-CSRF-TOKEN': '{{ csrf_token() }}',
+                    },
+                    body: JSON.stringify({
+                        startDate: startDate,
+                        endDate: endDate
+                    }),
+                })
+                .then(response => response.json())
+                .then(data => {
+                    var customSalesChartData = formatData(data.salesData);
+                    var ctxCustomSales = document.getElementById('customSalesChart').getContext('2d');
+                    drawChart(ctxCustomSales, customSalesChartData, `Custom Sales (${startDate} to ${endDate})`);
+
+
+                    document.getElementById('totalAmount').innerText = data.totalAmount;
+                })
+                .catch(error => {
+                    console.error('Error fetching custom sales data:', error);
+                });
+        };
+    </script>
 @endsection
