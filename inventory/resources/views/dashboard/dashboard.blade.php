ee072aafec224d53c2366c55b0aa7a55ad05761a
ArugaYudish <ayudish7@gmail.com>
2023-07-24 20:37:08 +0700

    aruga

--- inventory/resources/views/dashboard/dashboard.blade.php
+++ inventory/resources/views/dashboard/dashboard.blade.php
@@ -1,60 +1,165 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
 
-<div class="row justify-content-center mt-5">
-    <div class="w-100">
-        <div class="card">
-            <div class="card-header">Dashboard baru</div>
-            <div class="card-body">
-                @if ($message = Session::get('success'))
-                    <div class="alert alert-success">
-                        {{ $message }}
-                    </div>
-                @else
-                    <div class="alert alert-success">
-                        You are logged in!
-                    </div>       
-                @endif
+        <div class="row">
 
-                <div class="card mt-3">
-                    <div class="card-header">Total Customers</div>
+            <div class="col-xl-4 col-md-6 mb-4">
+                <div class="card border-left-primary shadow h-100 py-2">
                     <div class="card-body">
-                        <h1>{{ $customerCount }}</h1>
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                    Earnings (Monthly)</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
                     </div>
                 </div>
+            </div>
 
-                <div class="card mt-3">
-                    <div class="card-header">stock available</div>
+            <div class="col-xl-4 col-md-6 mb-4">
+                <div class="card border-left-success shadow h-100 py-2">
                     <div class="card-body">
-                        <h1>{{ $totalStock }}</h1>
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                    Earnings (Annual)</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
                     </div>
                 </div>
+            </div>
+
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-4 col-md-6 mb-4">
+                <div class="card border-left-info shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
+                                </div>
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col-auto">
+                                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="progress progress-sm mr-2">
+                                            <div class="progress-bar bg-info" role="progressbar" style="width: 50%"
+                                                aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-8 col-md-6 mb-4 row ml-3">
                 
-                <div class="card mt-3">
-                    <div class="card-header">Sold Products This Month</div>
-                    <div class="card-body">
-                        <h1>{{ $soldProductsThisMonth }}</h1>
+            </div>
+
+            <!--  Pending Requests Card Example -->
+            <div class="col-xl-4 col-md-6 mb-4 row ">
+                <div class="col-12  mb-4">
+                    <div class="card border-left-warning shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                        Pending Requests</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-comments fa-2x text-gray-300"></i>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
-                <div class="card mt-3">
-                    <div class="card-header">Total Sales This Month</div>
-                    <div class="card-body">
-                        <h1>{{ $totalSalesThisMonth }}</h1>
+                <div class="col-12 mb-4">
+                    <div class="card border-left-warning shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                        Pending Requests</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-comments fa-2x text-gray-300"></i>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
+            </div>
 
-                <div class="card mt-3">
-                    <div class="card-header">Total Production This Month</div>
-                    <div class="card-body">
-                        <h1>{{ $totalProductionThisMonth }}</h1>
+        </div>
+    </div>
+
+
+
+    <div class="row justify-content-center mt-5">
+        <div class="w-100 ">
+
+
+
+            <div class="card">
+                <div class="card-header">Dashboard baru</div>
+                <div class="card-body">
+
+
+                    <div class="card mt-3">
+                        <div class="card-header">Total Customers</div>
+                        <div class="card-body">
+                            <h1>{{ $customerCount }}</h1>
+                        </div>
                     </div>
-                </div>
 
+                    <div class="card mt-3">
+                        <div class="card-header">stock available</div>
+                        <div class="card-body">
+                            <h1>{{ $totalStock }}</h1>
+                        </div>
+                    </div>
+
+                    <div class="card mt-3">
+                        <div class="card-header">Sold Products This Month</div>
+                        <div class="card-body">
+                            <h1>{{ $soldProductsThisMonth }}</h1>
+                        </div>
+                    </div>
+
+                    <div class="card mt-3">
+                        <div class="card-header">Total Sales This Month</div>
+                        <div class="card-body">
+                            <h1>{{ $totalSalesThisMonth }}</h1>
+                        </div>
+                    </div>
+
+                    <div class="card mt-3">
+                        <div class="card-header">Total Production This Month</div>
+                        <div class="card-body">
+                            <h1>{{ $totalProductionThisMonth }}</h1>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
-    </div>    
-</div>
-    
+    </div>
+
+
 @endsection
