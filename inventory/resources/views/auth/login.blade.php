c9de421cb142db7600630507ee19ab0ee3696f55
ArugaYudish <ayudish7@gmail.com>
2023-07-25 11:02:46 +0700

    fixing Sidebar dan navbar

--- inventory/resources/views/auth/login.blade.php
+++ inventory/resources/views/auth/login.blade.php
@@ -1,9 +1,9 @@
-@extends('layouts.app')
+@extends('layouts.app2')
 
 @section('content')
 
-<div class="row justify-content-center mt-5">
-    <div class="col-md-8">
+<div class="row justify-content-center mt-5 py-4">
+    <div class="col-md-8 mt-5">
 
         <div class="card">
             <div class="card-header">Login</div>
