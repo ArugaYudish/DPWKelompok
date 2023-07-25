c412de3daa1bbb05ec2261bfa229e3de92ef654d
fayzaprlz19 <fayzapriliza@gmail.com>
2023-07-25 11:42:51 +0700

    fixing login page

--- inventory/resources/views/layouts/app.blade.php
+++ inventory/resources/views/layouts/app.blade.php
@@ -40,9 +40,9 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"></i>
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('dashboard') }}">
+                <div class="sidebar-brand-icon">
+                    <img src="{{ asset('style/img/logo-sidebar.png') }}" alt="">
                 </div>
                 <div class="sidebar-brand-text mx-3">Inventory</div>
             </a>
@@ -57,7 +57,7 @@
                     <span>Dashboard</span></a>
             </li> -->
 
-            <li class="nav-item {{ request()->routeIs('dashboard.dashboard') ? 'active' : '' }}">
+            <li class="nav-item {{ request()->routeIs('dashboard') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route('dashboard') }}">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
@@ -66,7 +66,7 @@
                 class="nav-item {{ request()->routeIs('products.index') || request()->routeIs('products.create') || request()->routeIs('products.edit') || request()->routeIs('products.show') ? 'active' : '' }}">
                 <a class="nav-link" href="{{ route('products.index') }}">
                     <i class="fas fa-fw fa-table"></i>
-                    <span>Data products</span></a>
+                    <span>Data Products</span></a>
             </li>
             <li
                 class="nav-item {{ request()->routeIs('customers.index') || request()->routeIs('customers.create') || request()->routeIs('customers.edit') || request()->routeIs('customers.show') ? 'active' : '' }}">
