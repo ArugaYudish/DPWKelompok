125d7785ed6051e795b2728c4228dbd7133790de
fayzaprlz19 <fayzapriliza@gmail.com>
2023-07-25 11:52:55 +0700

    fixing login page

--- inventory/resources/views/auth/login.blade.php
+++ inventory/resources/views/auth/login.blade.php
@@ -2,7 +2,63 @@
 
 @section('content')
 
-<div class="row justify-content-center mt-5 py-4">
+{{-- Ini yang baru --}}
+<div class="container">
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+        <div class="col-xl-10 col-lg-12 col-md-9">
+
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                        <div class="col-lg-6 d-none d-lg-block" style="background: url('{{ asset('style/img/logo.png') }}'); background-position: center; background-size: cover;"></div>
+                        <div class="col-lg-6">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                </div>
+                                <form action="{{ route('authenticate') }}" method="post">
+                                    @csrf
+                                    <div class="form-group row">
+                                        <label for="email" class="col-md-6 col-form-label text-md-end text-start">Email Address</label>
+                                        <div class="col-md-12">
+                                        <input type="email" class="form-control form-control-user @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}">
+                                            @if ($errors->has('email'))
+                                                <span class="text-danger">{{ $errors->first('email') }}</span>
+                                            @endif
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="password" class="col-md-4 col-form-label text-md-end text-start">Password</label>
+                                        <div class="col-md-12">
+                                        <input type="password" class="form-control form-control-user @error('password') is-invalid @enderror" id="password" name="password">
+                                            @if ($errors->has('password'))
+                                                <span class="text-danger">{{ $errors->first('password') }}</span>
+                                            @endif
+                                        </div>
+                                    </div>
+                                    <div class="mb-3 pt-4">
+                                        <input type="submit" class="col-md-12 btn btn-primary" value="Login">
+                                    </div>
+                                </form>
+                                <div class="text-center">
+                                    <a class="small" href="register.html">Create an Account!</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+
+{{-- <div class="row justify-content-center mt-5 py-4">
     <div class="col-md-8 mt-5">
 
         <div class="card">
@@ -36,6 +92,6 @@
             </div>
         </div>
     </div>    
-</div>
+</div> --}}
     
 @endsection
